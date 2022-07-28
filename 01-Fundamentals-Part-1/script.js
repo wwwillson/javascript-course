4ed7684e8a0acaaff8f14e35c99766bb6b1de776
wilson <wwwillson03@gmail.com>
2022-07-28 22:37:56 +0800

    1

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,82 @@
+/*let js = 'amazing';
+console.log(40+8+23-10); 
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+
+//Variable name conventions
+let joans_matilda = "JM";
+let $sunction = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun); 
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 34);
+// console.log(typeof 'Joans');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age =30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarch = now - 2018;
+console.log(ageJonas, ageSarch);
+
+console.log(ageJonas * 2, ageJonas /10, 2**3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schemdtmann';
+console.log(firstName + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarch);
+
+const isFullAge = ageSarch >= 18;
+console.log(now - 1991 > now - 2018);
+
+*/

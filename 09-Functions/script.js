4ea70d0482c9eb325f132b7d6f87ad2b25d60064
wilson <wwwillson03@gmail.com>
2022-08-19 21:25:09 +0800

    Bind method

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -115,3 +115,39 @@ book.apply(eurowings, flightData);
 console.log(eurowings);
 
 book.call(eurowings, ...flightData);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));

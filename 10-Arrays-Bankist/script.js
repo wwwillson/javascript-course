63434178cb437ee32569e99b96dada962416eff3
wilson <wwwillson03@gmail.com>
2022-08-26 21:30:23 +0800

    Challenge 1

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -164,3 +164,20 @@ currenciesUnique.forEach(function (value, key, map) {
 });
 
 */
+
+// Challenge 1
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);

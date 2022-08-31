72b6b58ec8706e243654098a92087a8041fd76b0
wilson <wwwillson03@gmail.com>
2022-08-31 22:30:48 +0800

    flat and flatMap

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -398,7 +398,6 @@ console.log(accounts);
 const account = accounts.find((acc) => acc.owner === 'Jessica Davis');
 console.log(account);
 
-*/
 // some and every
 console.log(movements);
 console.log(movements.includes(-130));
@@ -414,3 +413,22 @@ const deposit = (mov) => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+// flat and flatMap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const overalBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+const overalBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);

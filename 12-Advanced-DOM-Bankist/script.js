db2867e537fd2665e82b70108e9f00e19846a8de
wilson <wwwillson03@gmail.com>
2022-09-16 22:32:16 +0800

    Button scrolling

--- 12-Advanced-DOM-Bankist/script.js
+++ 12-Advanced-DOM-Bankist/script.js
@@ -30,7 +30,7 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-
+/*
 // Selecting, Creating, and Deleting Elements
 console.log(document.querySelectorAll('.section'));
 console.log(document.getElementsByTagName('button'));
@@ -95,3 +95,34 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 logo.clasName = 'jonas';
+
+*/
+
+// Button scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'hight/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
